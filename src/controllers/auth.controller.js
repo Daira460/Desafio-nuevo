72797db3cb625331aca11462053b5b7a418eef7d
DaiC <daira_02@hotmail.com>
2024-03-04 12:20:55 -0300

    desafio

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,9 +4,9 @@ const {createHash} = require ('../utils/cryp-password.util')
 const passport = require ('passport')
 const router = Router()
 
-router.post ('/', passport.authenticate('login', {failureRedirect: '/auth/fail-login'}) , async (req, res) => {
+router.post('/', passport.authenticate('login', { failureRedirect: '/auth/fail-login' }), async (req, res) => {
     try {
-        const {email} = req.body
+        const { email } = req.body;
         const lowercaseEmail = email.toLowerCase();
 
         req.session.user = {
@@ -15,13 +15,13 @@ router.post ('/', passport.authenticate('login', {failureRedirect: '/auth/fail-l
             email: lowercaseEmail,
             age: req.user.age,
             role: req.user.role,
-        }
-        res.json ({status: 'success', message: 'Login Succesfull'})
-     } catch (error) {
-        console.error ('Error:', error.message)
-        res.status(500).json({ error: 'Internal Server Error' })
+        };
+        res.json({ status: 'success', message: 'Login Succesfull' });
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
     }
-})
+});
 
 router.get('/current', (req, res) => {
     if (req.isAuthenticated()) {
@@ -32,9 +32,9 @@ router.get('/current', (req, res) => {
 });
 
 router.get('/fail-login', (req, res) => {
-    console.log ('Fallo el logueo')
-    res.status().json({status: 'error',  error: 'bad Request' })
-})
+    console.log('Fallo el logueo');
+    res.status().json({ status: 'error', error: 'bad Request' });
+});
 
 router.get('/logout', async (req, res) => {
     try {
@@ -46,30 +46,28 @@ router.get('/logout', async (req, res) => {
             }
         });
     } catch (error) {
-        console.error ('Error:', error.message)
-        res.status(500).json({ error: error })
+        console.error('Error:', error.message);
+        res.status(500).json({ error: error });
     }
-})
+});
 
 router.post('/forgotPassword', async (req, res) => {
     try {
-        const {email,password} = req.body
-        const passwordEncrypted = createHash(password)
-        await Users.updateOne ({email}, {password: passwordEncrypted})
-        res.status(200).json ({status: 'Success', message: 'Password Updated'})
+        const { email, password } = req.body;
+        const passwordEncrypted = createHash(password);
+        await Users.updateOne({ email }, { password: passwordEncrypted });
+        res.status(200).json({ status: 'Success', message: 'Password Updated' });
     } catch (error) {
-    console.error ('Error:', error.message)
-    res.status(500).json({ error: error })
+        console.error('Error:', error.message);
+        res.status(500).json({ error: error });
     }
-})
+});
 
-router.get('/github', passport.authenticate('github', {scope: ['user: email']}, (req, res) => {}))
+router.get('/github', passport.authenticate('github', { scope: ['user: email'] }));
 
-router.get('/githubcallback', passport.authenticate('github', {failureRedirect: '/login'}),
-    (req, res) => {
-    req.session.user = req.user
-    res.redirect('/profile')
-    }
-)
+router.get('/githubcallback', passport.authenticate('github', { failureRedirect: '/login' }), (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/profile');
+});
 
-module.exports = router
+module.exports = router;
