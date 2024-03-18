3a2e400983c2c1ff178a4d4ab09184b7f5530d85
DaiC <daira_02@hotmail.com>
2024-03-18 20:15:16 -0300

    Arreglos de 3 entrega y desafio nuevo

--- src/DAO/product-dao.mongo.js.js
+++ src/DAO/product-dao.mongo.js.js
@@ -8,20 +8,18 @@ class ProductDao {
           return await Products.findOne({ _id: id, status: true })
       } catch (error) {
           console.log('Error al obtener el producto por ID:', error.message)
-          throw error // Lanza la excepción para ser manejada en un nivel superior
+          throw error 
       } 
   }
 
     async addProduct(product) {
       try {
           const { title, description, price, thumbnail, code, stock, category } = product
-          //Valido que todos los campos son obligatorios
           if (!title || !description || !price || !code || !stock || !category) {
             console.error ("Todos los campos son obligatorios. Producto no agregado.")
             return { success: false, message: "Todos los campos son obligatorios. Producto no agregado." }
           }
 
-          //valido si ya existe el code
           const codeExist = await Products.findOne({ code: code})
           if (codeExist) {
             console.error (`El producto con code: ${code} ya existe. Por favor, seleccione otro.`)
@@ -30,10 +28,8 @@ class ProductDao {
 
           const NewProductInfo = new NewProductDto(product)
 
-          //creo el nuevo producto en la bd
           await Products.create(NewProductInfo)
 
-          // Retorna el éxito si se crea el producto
           return { success: true }
 
       } catch (error) {
@@ -44,7 +40,6 @@ class ProductDao {
 
     async updateProduct(productUpdated) {
         try {
-          // busco por id y realizo un update proporcionando el objeto completo con sus props a cambiar
           await Products.findOneAndUpdate( {_id: productUpdated.id}, productUpdated )
           if (!productUpdated.id) {
             console.error("Producto no encontrado con ID:", productUpdated.id)
@@ -58,8 +53,7 @@ class ProductDao {
       }
 
     async deleteProduct(id) {
-      try {
-          // busco por id y cambio el status a false, para hacer un soft Delete de producto.     
+      try {    
         const idExist = await Products.updateOne({ _id: id }, { $set: { status: false } })
         if (idExist) {
           console.log("Producto borrado correctamente")}
@@ -72,9 +66,8 @@ class ProductDao {
     
   async updateStock(productsInStock) {
     try {
-        // Itera sobre cada producto en productsInStock
         for (const product of productsInStock) {
-            // Encuentra el producto por su ID
+    
             const productId = product.product._id
             const quantity = product.quantity
 
@@ -84,10 +77,8 @@ class ProductDao {
                 throw new Error(`Producto con ID ${productId} no encontrado`)
             }
 
-            // Actualiza el stock restando la cantidad vendida
             foundProduct.stock -= quantity
 
-            // Guarda los cambios en la base de datos
             await foundProduct.save()
             console.log(`Stock del producto ${foundProduct.title} actualizado correctamente`)
         }
